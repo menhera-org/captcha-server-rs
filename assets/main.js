d5d233c593bc291f448e2c484249de76127b7de2
metastable-void <git@yuka.h09.eu>
2024-04-09 05:58:07 +0900

    fix js

--- assets/main.js
+++ assets/main.js
@@ -1,24 +1,28 @@
 
-const buttonSubmit = document.getElementById('button-submit');
-const inputRequestToken = document.getElementById('input-request-token');
-const inputRedirectUrl = document.getElementById('input-redirect-url');
-
-const url = new URL(window.location.href);
-const requestToken = url.searchParams.get('request-token');
-const redirectUrl = url.searchParams.get('redirect-url');
-
 let invalidRequest = false;
-if (requestToken) {
-    inputRequestToken.value = requestToken;
-} else {
-    invalidRequest = true;
-}
+let buttonSubmit;
 
-if (redirectUrl) {
-    inputRedirectUrl.value = redirectUrl;
-} else {
-    invalidRequest = true;
-}
+document.addEventListener('DOMContentLoaded', function() {
+    buttonSubmit = document.getElementById('button-submit');
+    const inputRequestToken = document.getElementById('input-request-token');
+    const inputRedirectUrl = document.getElementById('input-redirect-url');
+    
+    const url = new URL(window.location.href);
+    const requestToken = url.searchParams.get('request-token');
+    const redirectUrl = url.searchParams.get('redirect-url');
+    
+    if (requestToken) {
+        inputRequestToken.value = requestToken;
+    } else {
+        invalidRequest = true;
+    }
+    
+    if (redirectUrl) {
+        inputRedirectUrl.value = redirectUrl;
+    } else {
+        invalidRequest = true;
+    }
+});
 
 function captchaCallback(captchaToken) {
     if (invalidRequest) {
